44cfa31d71aba1f507ed6ae41dc14bb89e0ece4f
sihamelani <siham.elani17@gmail.com>
2024-04-11 13:21:03 +0000

    modification betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,28 +6,28 @@
  * integers in ascending order
  * @array: the array to print
  * @size: the size of array
- */ 
+ */
 void bubble_sort(int *array, size_t size)
 {
-  int temp;
-  size_t i, k;
-  if (array == NULL || size == 0)
-    {
-      return;
-    }
-  i = 0;
-  while (i < size)
-    {
-      for (k = 0; k < size - 1; k++)
-	{
-	  if (array[k] > array[k + 1])
-	    {
-	      temp = array[k];
-	      array[k] = array[k + 1];
-	      array[k + 1] = temp;
-	      print_array(array, size);
-	    }
-	}
-      i++;
-    }
+int temp;
+size_t i, k;
+if (array == NULL || size == 0)
+{
+return;
+}
+i = 0;
+while (i < size)
+{
+for (k = 0; k < size - 1; k++)
+{
+if (array[k] > array[k + 1])
+{
+temp = array[k];
+array[k] = array[k + 1];
+array[k + 1] = temp;
+print_array(array, size);
+}
+}
+i++;
+}
 }

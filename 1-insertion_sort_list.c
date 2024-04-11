16cc7d202feb1a39bb0bb2625439ec5c8a3c0df9
sihamelani <siham.elani17@gmail.com>
2024-04-11 14:26:55 +0000

    Task1 Write a function that sorts a doubly linked list of integers in ascending order

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,57 +1,56 @@
 #include "sort.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * swap - Function that get swap of linked list
- * @a: the adress of first node
- * @b: the adress of second node
+ * swap - Function that swaps two nodes of a linked list
+ * @a: the address of the first node
+ * @b: the address of the second node
  */
 void swap(listint_t *a, listint_t *b)
 {
-  if (a->prev != NULL)
-    {
-      a->prev->next = b;
-    }
-  if (b->next != NULL)
-    {
-      b->next->prev = a;
-    }
-  a->next = b->next;
-  b->prev = a->prev;
-  a->prev = b;
-  b->next = a;
+    if (a == NULL || b == NULL)
+        return;
+
+    if (a->prev != NULL)
+        a->prev->next = b;
+    if (b->next != NULL)
+        b->next->prev = a;
+    
+    a->next = b->next;
+    b->prev = a->prev;
+    a->prev = b;
+    b->next = a;
 }
 
 /**
  * insertion_sort_list - Function that sorts a doubly linked
  * list of integers in ascending order
- * @list: the list to print
+ * @list: the list to sort
  */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *i, *j;
- if (list == NULL || (*list) == NULL || (*list)->next == NULL)
-{
-return;
-}
- i = (*list)->next;
-while (i != NULL)
-{
-  j = i;
-  i = i->next;
-  while (j != NULL && j->prev != NULL)
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+
+    listint_t *current = (*list)->next;
+
+    while (current != NULL)
     {
-      if (j->prev->n > j->n)
-	{
-	  swap(j->prev, j);
-	  if (j->prev == NULL)
-	    {
-	      *list = j;
-	      print_list((const listint_t *)*list);
-	    }
-	}
-      else
-	j = j->prev;
+        listint_t *ptr = current;
+        current = current->next;
+
+        while (ptr != NULL && ptr->prev != NULL)
+        {
+            if (ptr->prev->n > ptr->n)
+            {
+                swap(ptr->prev, ptr);
+                if (ptr->prev == NULL)
+                    *list = ptr;
+                print_list(*list);
+            }
+            else
+                ptr = ptr->prev;
+        }
     }
-}
 }
